ecd0e2f1414f920a0292e8ede36237ba20827ebd
Nepumi-Jr <peerator@kkumail.com>
2021-12-22 20:22:43 +0700

    🚑️no unit

--- src/BigAnswerConvert.py
+++ src/BigAnswerConvert.py
@@ -12,8 +12,8 @@ def bigConvert(someContent: str):
 
     if ":" in someContent:
 
-        nameVar = someContent.strip().replace(" ", '').split(":")[0]
-        data = someContent.strip().replace(" ", '').split(":")[-1]
+        nameVar = someContent.strip().replace(" ", "").split(":")[0]
+        data = someContent.strip().replace(" ", "").split(":")[-1]
 
         res = EquationStuff.convertAndCheck(data)
         if type(res) == type("hello"):
@@ -21,30 +21,31 @@ def bigConvert(someContent: str):
 
         return ("E", nameVar, data)
     elif "=" in someContent:
-        nameVar = "=".join(
-            someContent.strip().replace(" ", '').split("=")[:-1])
-        dataStr = someContent.strip().replace(" ", '').split("=")[-1]
+        nameVar = "=".join(someContent.strip().replace(" ", "").split("=")[:-1])
+        dataStr = someContent.strip().replace(" ", "").split("=")[-1]
         data = VariableStuff.splitNumAndUnit(dataStr)
 
         if type(data) == type("error"):
             return "Warning {} : {}".format(nameVar, dataStr)
 
         val, unit = data
+        if unit.strip() == "":
+            return "Warning {} : {}".format(nameVar, "Unit not found")
 
         if unit[0] in "GMkmunp":
-            if unit[0] == 'G':
+            if unit[0] == "G":
                 val *= 1e9
-            elif unit[0] == 'M':
+            elif unit[0] == "M":
                 val *= 1e6
-            elif unit[0] == 'k':
+            elif unit[0] == "k":
                 val *= 1e3
-            elif unit[0] == 'm':
+            elif unit[0] == "m":
                 val *= 1e-3
-            elif unit[0] == 'u':
+            elif unit[0] == "u":
                 val *= 1e-6
-            elif unit[0] == 'n':
+            elif unit[0] == "n":
                 val *= 1e-9
-            elif unit[0] == 'p':
+            elif unit[0] == "p":
                 val *= 1e-12
             unit = unit[1:]
 
@@ -62,15 +63,5 @@ if __name__ == "__main__":
         else:
             print()
 
-    printWow(bigConvert("I1 = 1.2 A"))
-    printWow(bigConvert("Q1.1. magpp_vs = 20 V"))
-    printWow(bigConvert("Q1.2. freq_vs = 400 Hz"))
-    printWow(bigConvert("Q1.3. ZR = 100 ohm"))
-    printWow(bigConvert("Q1.4. ZC = - j 198.943679092 ohm"))
-    printWow(bigConvert("Q1.5. magpp_i = 0.089822 A"))
-    printWow(bigConvert("Q1.6. freq_i = 400 Hz"))
-    printWow(bigConvert("Q1.7. phase_i = 1.105027 rad"))
-    printWow(bigConvert("Q1.8. phase_i = 63.31339 deg"))
-    printWow(bigConvert("Q1.9. PR = 0.201699978039 W"))
-    printWow(bigConvert("Q1.10. PC = 0 W"))
-    printWow(bigConvert("I1 = 1.2 A"))
+    printWow(bigConvert("I1 = 1.2"))
+    # printWow(bigConvert(""))
--- src/LCAJudge.py
+++ src/LCAJudge.py
@@ -15,11 +15,10 @@
 """
 
 
-
 from os import path
 import os
 import sys
-from subprocess import Popen,TimeoutExpired,PIPE
+from subprocess import Popen, TimeoutExpired, PIPE
 import time
 import signal
 import EquationStuff
@@ -29,33 +28,36 @@ import BigAnswerConvert
 judgeArgs = sys.argv[-1]
 
 if not path.exists(judgeArgs):
-    print(f"!;0;1;0;0;Judge args not found :(",end = "")
+    print(f"!;0;1;0;0;Judge args not found :(", end="")
     exit(0)
 
 try:
-    with open(judgeArgs,"r") as f:
+    with open(judgeArgs, "r") as f:
         judgeArgs = f.read().split("\n")
 
 except:
-    print(f"!;0;1;0;0;Can't read Judge args:(",end = "")
+    print(f"!;0;1;0;0;Can't read Judge args:(", end="")
     exit(0)
 
-if(len(judgeArgs) < 9):
-    print(f"!;0;1;0;0;Not Enough info to judge\nexpected 9 args got {len(judgeArgs)} args",end = "")
+if len(judgeArgs) < 9:
+    print(
+        f"!;0;1;0;0;Not Enough info to judge\nexpected 9 args got {len(judgeArgs)} args",
+        end="",
+    )
     exit(0)
 
 try:
 
     testCase = judgeArgs[0] or ""
-    timeLimit = int(judgeArgs[1] or "")#In ms
-    memoryLimit = int(judgeArgs[2] or "")#mb
+    timeLimit = int(judgeArgs[1] or "")  # In ms
+    memoryLimit = int(judgeArgs[2] or "")  # mb
     PROBLEM_DIR = judgeArgs[3] or ""
 except:
-    print(f"!;0;1;0;0;Can't convert data :(",end = "")
+    print(f"!;0;1;0;0;Can't convert data :(", end="")
     exit(0)
 
-if(len(judgeArgs) < 6):
-    print(f"!;0;1;0;0;Program not Found",end = "")
+if len(judgeArgs) < 6:
+    print(f"!;0;1;0;0;Program not Found", end="")
     exit(0)
 
 
@@ -67,62 +69,65 @@ cmpArg = judgeArgs[6] or ""
 outMain = judgeArgs[7]
 outArg = judgeArgs[8]
 
+
 def DEBUG(x):
     if testCase != "2":
         print(f"D;0;1;0;0;{x}")
     else:
         print(f"E;0;1;0;0;ENDED")
-    exit(0) 
+    exit(0)
+
 
 def getNumTol():
 
     DEF_TOL = 0.001
 
-    if path.exists(path.join(PROBLEM_DIR,"numtol.txt")):
+    if path.exists(path.join(PROBLEM_DIR, "numtol.txt")):
         try:
-            with open(path.join(PROBLEM_DIR,"numtol.txt"),"r") as f:
+            with open(path.join(PROBLEM_DIR, "numtol.txt"), "r") as f:
                 tol = float(f.read().strip())
                 return abs(tol)
         except:
             return DEF_TOL
-    
+
     return DEF_TOL
 
 
-def writeLog(text:str):
-    with open(path.join(PROBLEM_DIR,f"{int(time.time())}T{testCase}LOG.txt"),"w") as f:
+def writeLog(text: str):
+    with open(
+        path.join(PROBLEM_DIR, f"{int(time.time())}T{testCase}LOG.txt"), "w"
+    ) as f:
         f.write(text)
 
 
-
-def str2CppCmp(x:str) -> list():
+def str2CppCmp(x: str) -> list():
     res = []
-    x = x.replace("\r","").split("\n")
+    x = x.replace("\r", "").split("\n")
     for e in x:
         for f in e.strip().split(" "):
             if f.strip():
                 res.append(f.strip())
-    
+
     return res
+
 
 ANS = []
 MAX_TEST = len(ANS)
 
+
 def readSolution():
 
-    global ANS,MAX_TEST
+    global ANS, MAX_TEST
 
-    if not path.exists(path.join(PROBLEM_DIR,"answer.txt")):
+    if not path.exists(path.join(PROBLEM_DIR, "answer.txt")):
         if testCase == "1":
-            print(f"!;0;1;0;0;answer.txt not found",end = "")
+            print(f"!;0;1;0;0;answer.txt not found", end="")
             exit(0)
         else:
-            print(f"E;0;0;0;0;End of Test",end = "")
+            print(f"E;0;0;0;0;End of Test", end="")
             exit(0)
-    with open(path.join(PROBLEM_DIR,"answer.txt"),"r") as f:
-        content =  f.read().strip().replace("\r","").split("\n")
-    
-    
+    with open(path.join(PROBLEM_DIR, "answer.txt"), "r") as f:
+        content = f.read().strip().replace("\r", "").split("\n")
 
     for line in content:
 
@@ -132,37 +137,44 @@ def readSolution():
 
     MAX_TEST = len(ANS)
 
+
 def variableCompare(data):
-    tc = int(testCase) - 1 
+    tc = int(testCase) - 1
     tol = getNumTol()
 
-    if VariableStuff.compareVar(data[2] , ANS[tc][2], tol) and data[3].lower() == ANS[tc][3].lower():
-        return "P",1,1,"Wow za!"
+    if (
+        VariableStuff.compareVar(data[2], ANS[tc][2], tol)
+        and data[3].lower() == ANS[tc][3].lower()
+    ):
+        return "P", 1, 1, "Wow za!"
     else:
-        return "-",0,1,f"{data[1]} : Wrong Answer"
+        return "-", 0, 1, f"{data[1]} : Wrong Answer"
+
 
 def equationCompare(data):
     tol = getNumTol()
-    if EquationStuff.compareEqual(data[2],ANS[int(testCase) - 1][2],tol):
-        return "P",1,1,"Complete"
+    if EquationStuff.compareEqual(data[2], ANS[int(testCase) - 1][2], tol):
+        return "P", 1, 1, "Complete"
     else:
-        return "-",0,1,f"{data[1]} : Wrong answer"
-            
+        return "-", 0, 1, f"{data[1]} : Wrong answer"
+
 
 def compare():
 
     userStr = ""
     try:
-        with open(srcPath,"r") as f:
-            userStr = f.read().strip().replace("\r","").split("\n")
+        with open(srcPath, "r") as f:
+            userStr = f.read().strip().replace("\r", "").split("\n")
     except:
-        return "!",0,1,"Answer not found :("
+        return "!", 0, 1, "Answer not found :("
 
-    tc = int(testCase) - 1    
+    tc = int(testCase) - 1
 
     for line in userStr:
+        if line.strip() == "":
+            continue
         data = BigAnswerConvert.bigConvert(line.strip())
-        tc = int(testCase) - 1 
+        tc = int(testCase) - 1
 
         if type(data) == type("Hello"):
             continue
@@ -170,39 +182,41 @@ def compare():
         if data[1] == ANS[tc][1]:
 
             if ANS[tc][0] != data[0]:
-                return "X",0,1,f"{ANS[tc][1]} : Expected {ANS[tc][0] == 'E' and 'Equation' or 'Number'} got {data[0] == 'E' and 'Equation' or 'Number'}"
+                return (
+                    "X",
+                    0,
+                    1,
+                    f"{ANS[tc][1]} : Expected {ANS[tc][0] == 'E' and 'Equation' or 'Number'} got {data[0] == 'E' and 'Equation' or 'Number'}",
+                )
 
-            if data[0] == 'V':
+            if data[0] == "V":
                 return variableCompare(data)
             elif data[0] == "E":
                 return equationCompare(data)
 
-    return "X",0,1,f"{ANS[tc][1]} not found or Wrong format... :("
-
-    
+    return "X", 0, 1, f"{ANS[tc][1]} not found or Invalid format... :("
 
 
-#This is from Kiyago's standard judge
+# This is from Kiyago's standard judge
 def main():
-
 
     readSolution()
 
     if int(testCase) > MAX_TEST:
-        print(f"E;0;0;0;0;End of Test",end = "")
+        print(f"E;0;0;0;0;End of Test", end="")
         return
 
-    verdic,score,maxscore,comment = compare()
-
+    verdic, score, maxscore, comment = compare()
 
     # Clean up tmp directory
     try:
-        #if(path.exists(outPath)):os.remove(outPath)
-        #if(path.exists(errPath)):os.remove(errPath)
+        # if(path.exists(outPath)):os.remove(outPath)
+        # if(path.exists(errPath)):os.remove(errPath)
         pass
     except:
         pass
 
-    print(f"{verdic};{score};{maxscore};0;0;{comment}",end = "")
+    print(f"{verdic};{score};{maxscore};0;0;{comment}", end="")
+
 
 main()
